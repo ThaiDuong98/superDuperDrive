2d95900edc66e4f00130cea7b346c143810cd366
duongpham <thaiduong6898@gmail.com>
2024-09-06 22:47:53 +0700

    Add mapper interface

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/CredentialMapper.java
@@ -0,0 +1,26 @@
+package com.udacity.jwdnd.course1.cloudstorage.mapper;
+
+import com.udacity.jwdnd.course1.cloudstorage.entity.Credential;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface CredentialMapper {
+    @Select("SELECT * FROM CREDENTIALS WHERE userId = #{userId}")
+    List<Credential> getAllCredentials(Integer userId);
+
+    @Select("SELECT * FROM CREDENTIALS WHERE credentialId=#{credentialId}")
+    Credential getCredentialById(Integer credentialId);
+
+    @Insert("INSERT INTO CREDENTIALS (credentialId, url, username, key, password, userId) " +
+            "VALUES (#{credentialId}, #{url}, #{username}, #{key}, #{password}, #{userId})")
+    @Options(useGeneratedKeys = true, keyProperty = "credentialId")
+    int insertCredential(Credential credential);
+
+    @Update("UPDATE CREDENTIALS SET url=#{url}, username=#{username}, key=#{key}, password=#{password} " +
+            "WHERE credentialId = #{credentialId}")
+    int updateCredential(Credential credential);
+
+
+}
