8f1e173b9dd0a83d573d2428137e0c140c5d681c
duongpham <thaiduong6898@gmail.com>
2024-09-08 07:56:17 +0700

    Update validate maxsize off file

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FileController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FileController.java
@@ -33,7 +33,7 @@ public class FileController {
         }
 
         if(fileUpload.getSize() > MAX_SIZE_FILE){
-            model.addAttribute("errorMessage", "Maximum size is 10MB, Please try again");
+            model.addAttribute("errorMessage", "Maximum size of file is 10MB, Please try again");
             return "result";
         }
 
