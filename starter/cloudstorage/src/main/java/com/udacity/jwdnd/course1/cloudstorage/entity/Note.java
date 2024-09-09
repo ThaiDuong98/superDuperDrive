1c3bb5befc3bc0dec44c898e62ff5666ef72950f
duongpham <thaiduong6898@gmail.com>
2024-09-09 23:15:58 +0700

    fix note entity

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/entity/Note.java
@@ -0,0 +1,17 @@
+package com.udacity.jwdnd.course1.cloudstorage.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Note {
+    private Integer noteId;
+    private String noteTitle;
+    private String noteDescription;
+    private Integer userId;
+}
